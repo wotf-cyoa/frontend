0d851a795a26c4620a4905918bac0bf7e671cea3
mgarbacz <mich.garbacz@gmail.com>
2014-06-06 01:23:11 -0500

    Basic hacky socket server that runs Ruby scripts

---
+++ server.js
@@ -0,0 +1,24 @@
+var app = require('http').createServer(),
+    io = require('socket.io').listen(app),
+    exec = require('child_process').execFile;
+
+var tones = io.of('/ruby').on('connection', function(socket) {
+    socket.on('scriptIn', function(data) {
+        console.log(data);
+
+        exec('ruby', ['-e', data.script], function (err, stdout, stderr) {
+            console.log(stdout);
+            console.log(err || stderr);
+
+            socket.emit('scriptOut', {
+                result: stdout,
+                error: err || stderr
+            });
+        });
+    });
+    socket.emit('ready', {
+        result: 'Enter valid ruby code...'
+    });
+});
+
+app.listen(process.env.PORT || 8888);
