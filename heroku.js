74e80593d1d3cca4bbb8c27acbc481a238411676
mgarbacz <mich.garbacz@gmail.com>
2014-08-04 08:20:46 -0500

    Add brunch build to heroku express server

--- heroku.js
+++ heroku.js
@@ -1,5 +1,12 @@
-var express = require('express');
-var app = express();
+var express = require('express'),
+    exec = require('child_process').exec,
+    app = express();
+
+exec('brunch b', function(error, stdout, stderr) {
+    console.log(error);
+    console.log(stdout);
+    console.log(stderr);
+});
 
 // Routing
 app.use(express.static(__dirname + '/public'));
